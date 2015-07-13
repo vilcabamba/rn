220b3b852cc69856bd008aa580a06253bfc8fe8e
Mario Andrés Correa <a@macool.me>
2015-07-13 00:27:38 -0500

    migrate db before compiling assets

--- update.bash
+++ update.bash
@@ -2,6 +2,6 @@
 git fetch
 git pull --rebase origin master
 export RAILS_ENV=production
-bundle exec rake assets:precompile
 bundle exec rake db:migrate
+bundle exec rake assets:precompile
 touch tmp/restart.txt
