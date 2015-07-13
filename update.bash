6a6245cf77a6e4da469dc72cc43b0bab4383a90f
Mario Andrés Correa <a@macool.me>
2015-07-13 00:28:41 -0500

    bundle on deploy

--- update.bash
+++ update.bash
@@ -2,6 +2,7 @@
 git fetch
 git pull --rebase origin master
 export RAILS_ENV=production
+bundle install --deployment
 bundle exec rake db:migrate
 bundle exec rake assets:precompile
 touch tmp/restart.txt
