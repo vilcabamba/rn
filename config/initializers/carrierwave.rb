b6177839e322420cf642bbef3610eacc6e3a2834
Mario Andrés Correa <a@macool.me>
2015-08-23 13:25:24 -0500

    try to fix carrierwave protocol

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,6 @@
+protocol = Rails.application.config.force_ssl ? "https://" : "http://"
+host = ActionController::Base.asset_host
+
 CarrierWave.configure do |config|
-  config.asset_host = ActionController::Base.asset_host
+  config.asset_host = protocol + host
 end
