0daea07024530d89d0bbbb92eb2ca6a572bc708f
Mario Andrés Correa <a@macool.me>
2015-09-05 12:48:44 -0500

    add gauges

---
+++ app/assets/javascripts/public/gauges.js
@@ -0,0 +1,16 @@
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '55eb2a54de2e2621530048a0');
+  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+  t.src = 'https://track.gaug.es/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+})();
+
+$(document).on("ready page:load", function () {
+  _gauges.push(['track']);
+});
